a456cc13ead52f72278297d04f09157140bd5d76
KMaster90 <73232282+KMaster90@users.noreply.github.com>
2022-06-01 15:19:45 +0200

    Add files via upload

---
+++ Project/app/app.module.ts
@@ -0,0 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { AgGridModule } from 'ag-grid-angular';
+import { AppComponent } from './app.component';
+import { PersonFilter } from './person-filter.component';
+import { YearFilter } from './year-filter.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    AgGridModule.withComponents([PersonFilter, YearFilter]),
+  ],
+  declarations: [AppComponent, PersonFilter, YearFilter],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
